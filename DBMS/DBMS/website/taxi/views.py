85bc0f34b8bf4affa05177c880d1f1fa649370e3
Raghav-Sahni <sahniraghavdlf@gmail.com>
2022-04-23 23:10:38 +0530

    made it so that null inputs can be taken and can search on multiple or select fields

--- DBMS/DBMS/website/taxi/views.py
+++ DBMS/DBMS/website/taxi/views.py
@@ -10,28 +10,28 @@ from .models import User, Driver, BillDetails, CarManufacturer, CustomerService,
 class BillForm(forms.Form):
     bill_no = forms.IntegerField(label="Bill Number", required=False)
     user_id = forms.IntegerField(label="User_ID", required=False)
-    amt = forms.IntegerField(label="Total Amount", required=False)#add required=False to all
+    amt = forms.IntegerField(label="Total Amount", required=False)
 
 class DriverForm(forms.Form):
-    trip_id = forms.IntegerField(label="Trip ID")
-    name = forms.CharField(label = "Name")
-    age = forms.IntegerField(label="Age")
-    rating = forms.IntegerField(label="Rating")
-    driver_id = forms.IntegerField(label="Driver ID")
+    trip_id = forms.IntegerField(label="Trip ID", required=False)
+    name = forms.CharField(label = "Name", required=False)
+    age = forms.IntegerField(label="Age", required=False)
+    rating = forms.IntegerField(label="Rating", required=False)
+    driver_id = forms.IntegerField(label="Driver ID", required=False)
 
 class TaxiForm(forms.Form):
-    driver_id = forms.IntegerField(label = "Driver ID")
-    name = forms.CharField(label = "Name")
-    car_id = forms.IntegerField(label="Car ID")
+    driver_id = forms.IntegerField(label = "Driver ID", required=False)
+    name = forms.CharField(label = "Name", required=False)
+    car_id = forms.IntegerField(label="Car ID", required=False)
 
 class TripForm(forms.Form):
-    start = forms.CharField(label = "Start Location")
-    end = forms.CharField(label = "Destination")
-    bill_no = forms.IntegerField(label = "Bill Number")
+    start = forms.CharField(label = "Start Location", required=False)
+    end = forms.CharField(label = "Destination", required=False)
+    bill_no = forms.IntegerField(label = "Bill Number", required=False)
 
 class UserForm(forms.Form):
-    name = forms.CharField(label = "Name")
-    user_id = forms.IntegerField(label = "User ID")
+    name = forms.CharField(label = "Name", required=False)
+    user_id = forms.IntegerField(label = "User ID", required=False)
 
 # Create your views here.
 def index(request):
@@ -42,11 +42,17 @@ def bill(request):
         form = BillForm(request.POST) #TO get data that user has submitted
         if form.is_valid(): #If submition is valid
             data = {"bill_no": form.cleaned_data["bill_no"], "user_id": form.cleaned_data["user_id"], "amount": form.cleaned_data["amt"]} #TaskForm stores task input in tasks variable
-            # Now need to rectify for if null then dont search on that query
-            b = BillDetails.objects.filter(bill_no = data["bill_no"], tot_amt = data["amount"], userid = data["user_id"])
+            
+            b = BillDetails.objects.all()
+            if data["bill_no"] != None:
+                b = b.filter(bill_no = data["bill_no"])
+            if data["user_id"] != None:
+                b = b.filter(userid = data["user_id"])
+            if data["amount"] != None:
+                b = b.filter(tot_amount = data["amount"])
             return render(request, "taxi/table.html", {
                 "data": b
-            }) # Directs me back to view tasks after submiting query
+            }) 
         else:# For server side validation, sometimes when we make a change here but client page has not been refreshed then the old version of the form may take invalid inputs this block is to prevent that
             return render(request, "taxi/search_bill.html", {
                 "form": form
@@ -61,8 +67,19 @@ def driver(request):
         form = DriverForm(request.POST) #TO get data that user has submitted
         if form.is_valid(): #If submition is valid
             data = {"trip_id": form.cleaned_data["trip_id"], "name": form.cleaned_data["name"], "age": form.cleaned_data["age"], "rating": form.cleaned_data["rating"], "driver_id": form.cleaned_data["driver_id"]} #TaskForm stores task input in tasks variable
-            #request.session["tasks"]+=[task]
-            return HttpResponseRedirect(reverse("taxi:index")) # Directs me back to view tasks after submiting query
+            #Still need to work on trip_id
+            b = Driver.objects.all()
+            if data["name"] != None:
+                b = b.filter(name = data["name"])
+            if data["age"] != None:
+                b = b.filter(age = data["age"])
+            if data["rating"] != None:
+                b = b.filter(rating = data["rating"])
+            if data["driver_id"] != None:
+                b = b.filter(driver_id = data["driver_id"])
+            return render(request, "taxi/table.html", {
+                "data": b
+            })
         else:# For server side validation, sometimes when we make a change here but client page has not been refreshed then the old version of the form may take invalid inputs this block is to prevent that
             return render(request, "taxi/search_bill.html", {
                 "form": form
@@ -77,8 +94,21 @@ def taxi(request):
         form = TaxiForm(request.POST) #TO get data that user has submitted
         if form.is_valid(): #If submition is valid
             data = {"driver_id": form.cleaned_data["driver_id"], "name": form.cleaned_data["name"], "car_id": form.cleaned_data["car_id"]} #TaskForm stores task input in tasks variable
-            #request.session["tasks"]+=[task]
-            return HttpResponseRedirect(reverse("taxi:index")) # Directs me back to view tasks after submiting query
+            #Still need to work name
+            b = Driver.objects.all()
+            t = Taxi.objects.all()
+            if data["name"] != None:
+                b = b.filter(name = data["name"])
+                #t = t.filter(driver = b.values_list("driver_id"))
+
+            if data["driver_id"] != None:
+                t = t.filter(driver = data["driver_id"])
+            if data["car_id"] != None:
+                t = t.filter(car_id = data["car_id"])
+            return render(request, "taxi/table.html", {
+                "data": t
+            })
+            
         else:# For server side validation, sometimes when we make a change here but client page has not been refreshed then the old version of the form may take invalid inputs this block is to prevent that
             return render(request, "taxi/search_taxi.html", {
                 "form": form
@@ -93,8 +123,16 @@ def trip(request):
         form = TripForm(request.POST) #TO get data that user has submitted
         if form.is_valid(): #If submition is valid
             data = {"start": form.cleaned_data["start"], "end": form.cleaned_data["end"], "bill_no": form.cleaned_data["bill_no"]} #TaskForm stores task input in tasks variable
-            #request.session["tasks"]+=[task]
-            return HttpResponseRedirect(reverse("taxi:index")) # Directs me back to view tasks after submiting query
+            #Work on bill_no
+            
+            b = TripDetails.objects.all()
+            if data["start"] != None:
+                b = b.filter(startlocation = data["start"])
+            if data["end"] != None:
+                b = b.filter(destination = data["end"])
+            return render(request, "taxi/table.html", {
+                "data": b
+            })
         else:# For server side validation, sometimes when we make a change here but client page has not been refreshed then the old version of the form may take invalid inputs this block is to prevent that
             return render(request, "taxi/search_trip.html", {
                 "form": form
@@ -109,8 +147,15 @@ def user(request):
         form = UserForm(request.POST) #TO get data that user has submitted
         if form.is_valid(): #If submition is valid
             data = {"name": form.cleaned_data["name"], "user_id":form.cleaned_data["user_id"]} #TaskForm stores task input in tasks variable
-            #request.session["tasks"]+=[task]
-            return HttpResponseRedirect(reverse("taxi:index")) # Directs me back to view tasks after submiting query
+
+            b = User.objects.all()
+            if data["name"] != None:
+                b = b.filter(name = data["name"])
+            if data["user_id"] != None:
+                b = b.filter(userid = data["user_id"])
+            return render(request, "taxi/table.html", {
+                "data": b
+            })
         else:# For server side validation, sometimes when we make a change here but client page has not been refreshed then the old version of the form may take invalid inputs this block is to prevent that
             return render(request, "taxi/search_user.html", {
                 "form": form
